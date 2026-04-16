d7f8de54641d5a7e64416e94ead70d9f7295817e
Marcos <111812112+Marcos33330@users.noreply.github.com>
2026-04-16 00:33:11 +0000

    proyecto de java listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-import "bootstrap";
-import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+    
+  let pronoun = ['the', 'our', 'his'];
+  let adj = ['great', 'big', 'amazing' ];
+  let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
+  
+  
+  for(let i = 0; i < pronoun.length; i++) {
+    for(let j = 0; j < adj.length; j++) {
+      for(let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
-};
+
